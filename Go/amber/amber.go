438734d16511634190c62a2007143b9054b8ae3b
Thomas Joseph <josept02@kefel.mssm.edu>
2009-11-16 23:08:04 -0500

    Move crdpipe functions to amber package

--- Go/amber/amber.go
+++ Go/amber/amber.go
@@ -14,6 +14,7 @@ import (
     "compress/gzip";
     "malloc";
 )
+import ( "what"; )
 
 // Constants for POINTERS block indices in prmtops, stolen from the prmtop 
 // format spec on ambermd.org
@@ -467,6 +468,60 @@ func DumpPairVectorAsText(v *vector.Vector, filename string) {
         p := v.At(i).(Pair);
         fmt.Fprintf(fp, "%d %d\n", p.Row, p.Col);
     }
+}
+
+// Dumps an array of coordinates in rst format.
+func DumpCoordsAsRst(coords []float32) {
+    fmt.Printf("We are the champions\n%d\n", len(coords)/3);
+    for i := 0; i < len(coords); i++ {
+        if i > 0 && i % 6 == 0 { fmt.Printf("\n") }
+        fmt.Printf("%12.6f", coords[i]);
+    }
+}
+
+// Reads bytes from an open file.
+func readLineFromOpenFile(fp *os.File) string {
+    buf := make([]byte, 256);
+    var i int;
+    fp.Read(buf[0:1]);
+    for i = 1; i < len(buf) && buf[i-1] != '\n'; i++ { fp.Read(buf[i:i+1]) }
+    return string(buf[0:i]);
+}
+
+// Gets an arbitrary frame from an AMBER text-format trajectory without having to
+// read in the entire thing first.
+func GetFrameFromTrajectory(filename string, frame, numAtoms int, hasBox bool) []float32 {
+    fp, err := os.Open(filename, os.O_RDONLY, 0);
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "Couldn't find file %s\n", filename);
+        return nil;
+    }
+    defer fp.Close();
+    
+    // Eat header
+    readLineFromOpenFile(fp);
+    // In an mdcrd trajectory, there are 10 coordinates per line,
+    // but in a single snapshot file there are 6, for some reason.
+    linesPerFrameTrj := int(math.Ceil(float64(numAtoms*3) / 10));
+    // There are as many newlines as lines per frame.
+    // 8 bytes per coordinate, 8*3+2+1 bytes for box
+    bytesPerFrame := numAtoms*8*3 + linesPerFrameTrj;
+    if hasBox { bytesPerFrame += 8*3+2+1 }
+    //    fmt.Fprintf(os.Stderr, "Bytes per frame: %d, lines per frame: %d; seeking to %d\n", bytesPerFrame, 
+    //        linesPerFrameTrj, frame * bytesPerFrame);
+    fp.Seek(int64(frame * bytesPerFrame), 1); // Seek relative to current offset
+    // fmt.Printf("%s\n%d\n", header, numAtoms);
+    // Read coordinates
+    coords := make([]float32, numAtoms*3);
+    buf := make([]byte, 8);
+    for i := 0; i < numAtoms*3; i++ {
+        // Eat newline every 10 coordinates
+        if i > 0 && i % 10 == 0 { fp.Read(buf[0:1]) }
+        fp.Read(buf);
+        coords[i] = float32(what.Atof64(string(buf)));
+        // if i < 150 { fmt.Printf("%f\n", coords[i]) }
+    }
+    return coords;
 }
 
 // Prints something about the current state of the program.
