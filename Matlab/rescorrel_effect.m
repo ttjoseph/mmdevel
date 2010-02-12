9f2d059619d4ff25c81966c5776c4a66b0c816fc
Thomas Joseph <josept02@kefel.mssm.edu>
2010-02-12 11:14:09 -0500

    Eh, I dunno

--- Matlab/rescorrel_effect.m
+++ Matlab/rescorrel_effect.m
@@ -2,8 +2,8 @@ function [data] = rescorrel_effect(rc, a, b)
 % Returns a plottable dataset of the "correlation" between each residue in a
 % with all of those in b, according to rescorrel matrix rc
 
-data = zeros(length(a));
+data = zeros(length(a),1);
 
 for i = a
-    data(i) = sum(rc(b));
+    data(i) = sum(rc(i,b));
 end
