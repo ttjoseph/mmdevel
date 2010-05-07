c7b35031d683f16f8b29ca9b7a54bea7e8a8d536
Tom Joseph <ttjoseph@gmail.com>
2010-05-06 22:00:05 -0400

    Add similarity measure for rescorrel matrices

---
+++ Matlab/rescorrel_similarity.m
@@ -0,0 +1,27 @@
+function [s] = rescorrel_similarity(rc1, rc2)
+%RESCORREL_SIMILARITY   Calculate similarity or whatever.
+%   If either of two corresponding cells have a value greater than the x-th percentile
+%   value, then the other one must also exceed its x-th percentile value for the pair
+%   to be considered similar. If neither cell has a value greater than threshold, the
+%   pair is not considered.
+%
+%   Returns a fraction representing similarity, with a maximum of 1.
+%   This result should be compared with 
+%
+%   Tom Joseph <thomas.joseph@mssm.edu>
+
+if size(rc1) ~= size(rc2)
+    error('Sizes of rescorrel matrices must be the same.')
+end
+
+percentile = 0.75;
+
+rc1v = sort(rc1(:));
+rc2v = sort(rc2(:));
+rc1_upper = rc1v(floor(length(rc1v) * percentile + 1));
+rc2_upper = rc2v(floor(length(rc2v) * percentile + 1));
+
+m = (rc1 > rc1_upper) & (rc2 > rc2_upper);
+m2 = (rc1 > rc1_upper) | (rc2 > rc2_upper);
+imagesc(m)
+s = sum(m(:)) / sum(m2(:));
