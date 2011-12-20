23527d416f7a13ff2fd6b535f0c41b2881c0b174
Tom Joseph <ttjoseph@gmail.com>
2011-12-20 12:06:21 -0500

    Fix name of function in rescorrel_factor.m

--- Matlab/rescorrel_factor.m
+++ Matlab/rescorrel_factor.m
@@ -1,4 +1,4 @@
-function [data] = rescorrel_effect(rc, a, b)
+function [data] = rescorrel_factor(rc, a, b)
 % Returns a plottable dataset of the "correlation" between each residue in a
 % with all of those in b, according to rescorrel matrix rc
 
