d39bedb507895c7d3126b47eb9c3e4faec89ea16
Tom Joseph <ttjoseph@gmail.com>
2012-01-04 13:25:05 -0500

    Normalize rescorrel matrices to use units of standard deviation

--- Matlab/sensor_residues2.m
+++ Matlab/sensor_residues2.m
@@ -1,3 +1,6 @@
+% Alternative method for finding sensor residues in TtAgo
+
+% Load files
 fm = load('-ascii', 'MD/FullyMatched/rescorrel.txt'); fm = fm(25:709, 25:709);
 g3t = load('-ascii', 'MD/G3T/rescorrel.txt'); g3t = g3t(25:709, 25:709);
 a4c = load('-ascii', 'MD/A4C/rescorrel.txt'); a4c = a4c(25:709, 25:709);
@@ -5,20 +8,31 @@ a4t = load('-ascii', 'MD/A4T/rescorrel.txt'); a4t = a4t(25:709, 25:709);
 g6c = load('-ascii', 'MD/G6C/rescorrel.txt'); g6c = g6c(25:709, 25:709);
 t7g = load('-ascii', 'MD/T7G/rescorrel.txt'); t7g = t7g(25:709, 25:709);
 
+% Calculate differences
 NUM_MISMATCHES = 5;
 dg3t = fm - g3t;
 da4c = fm - a4c;
 da4t = fm - a4t;
 dg6c = fm - g6c;
 dt7g = fm - t7g;
+
+% Use sigma units
+dg3t = dg3t / std(dg3t(:));
+da4c = da4c / std(da4c(:));
+da4t = da4t / std(da4t(:));
+dg6c = dg6c / std(dg6c(:));
+dt7g = dt7g / std(dt7g(:));
+
+% Descriptive statistics
 dsum = (dg3t + da4c + da4t + dg6c + dt7g);
 dsumsq = (dg3t.^2 + da4c.^2 + da4t.^2 + dg6c.^2 + dt7g.^2);
 dmean = dsum / NUM_MISMATCHES;
 % Variance for each cell in the matrix
 dvar = (dsumsq - (dsum.*dmean)) / (NUM_MISMATCHES - 1);
 
-% Sum each column to produce a list of values associated with each residue
-dmeansum = sum(dmean);
+% Root mean square (more informative than a straight sum) since opposite signs can't cancel
+sumdrms = sum(sqrt(dsumsq / NUM_MISMATCHES));
+sensor_residues = find(sumdrms > (mean(sumdrms) + 2*std(sumdrms)))
 
 % How do we know which residues are relevant?
 
