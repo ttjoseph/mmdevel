ec82df6203a726832c5f676a84e80e016de5cf35
Thomas Joseph <ttjoseph@gmail.com>
2014-12-20 21:14:58 -0500

    Unbreak center-and-fit.sh to not garble the output

--- GLIC/center-and-fit.sh
+++ GLIC/center-and-fit.sh
@@ -2,7 +2,8 @@
 i=$1
 inp="1
 0"
-echo $inp | trjconv -s $i.tpr -f $i.xtc -o $i.1.tmp.xtc -pbc nojump -ur compact -center
-echo $inp | trjconv -s $i.tpr -f $i.1.tmp.xtc -o $i.2.tmp.xtc -fit rot+trans
-echo $inp | trjconv -s $i.tpr -f $i.2.tmp.xtc -o $i.nopbc.fit.xtc -pbc mol -ur compact -center
+
+echo $inp | trjconv -s $i.tpr -f $i.xtc -o $i.1.tmp.xtc -pbc nojump -center
+echo 0 | trjconv -s $i.tpr -f $i.1.tmp.xtc -o $i.2.tmp.xtc -pbc res
+echo $inp | trjconv -s $i.tpr -f $i.2.tmp.xtc -o $i.nopbc.fit.xtc -fit rot+trans
 rm -f $i.1.tmp.xtc $i.2.tmp.xtc
