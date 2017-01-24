1e128deb94e8613ade96dcac04de6625c8eb8e28
Thomas Joseph <ttjoseph@gmail.com>
2017-01-24 13:14:54 -0500

    Add script to make gnuplot plots for FEP calculations

---
+++ make_fepenergy_gnuplot.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+FEP_BASENAME=$1
+shift
+
+python ~/mmdevel/fepout_cat.py ${FEP_BASENAME}*.fepout > $FEP_BASENAME.all.fepout
+grep FepEnergy $FEP_BASENAME.all.fepout | awk '{print $10}' > $FEP_BASENAME.fepenergy.gnuplot
+# 8 12
+cat ${FEP_BASENAME}*.fepout | grep '#Free' | awk '{print $8" "$12}' > $FEP_BASENAME.delta.gnuplot
+
+gnuplot <<FOO
+set terminal svg
+set output "$FEP_BASENAME.fepenergy.svg"
+plot "$FEP_BASENAME.fepenergy.gnuplot" w l
+
+set output "$FEP_BASENAME.delta.svg"
+plot "$FEP_BASENAME.delta.gnuplot" smooth cumulative
+FOO
