d9637555085f888b3577e82d6178657013bd18e2
Tom Joseph <ttjoseph@gmail.com>
2017-03-10 16:15:59 -0500

    More better ffparam_chargeopt

--- ffparam_chargeopt.py
+++ ffparam_chargeopt.py
@@ -4,6 +4,7 @@ import argparse
 import re
 import yaml
 from psf_prm_to_bin import get_psf_block_header, read_int_block
+import csv
 
 # Constants lifted from FFTK
 HARTREES_PER_KCAL = 1.041308e-21
@@ -16,7 +17,10 @@ COULOMB = 332.0636
 def parse_waterint_log(filename):
     """Extracts SCF energies from a water interaction calculation done by Gaussian."""
     lines = open(filename).readlines()
-    energies, coords = [], []
+    energies, coords, optimized_params = [], [], []
+    current_frame_coords = []
+    current_optimized_params = {}
+    optimized = False
     i = 0
     while i < len(lines):
         if re.search(r'Error termination', lines[i]):
@@ -27,12 +31,19 @@ def parse_waterint_log(filename):
             # All is well
             # sys.stderr.write('Gaussian did not complain in %s. Nice!\n' % filename)
             break
+            
+        # Extract parameters that were optimized, such as distances and angles
+        if re.search(r'Optimized Parameters', lines[i]):
+            i += 5
+            current_optimized_params = {}
+            optimized = True
+            while lines[i][1] != '-':
+                tokens = lines[i].split()
+                current_optimized_params[tokens[1]] = float(tokens[2])
+                i += 1
         
-        # Here, FFTK parses a relaxed potential scan...do we need to?
-        # XXX: For now we'll not worry about it
-
-        if re.search(r'Standard orientation:', lines[i]):
-            # Save the input coordinates for this piece of the dihedral scan
+        if re.search(r'Standard orientation:', lines[i]) or re.search(r'Z-Matrix orientation:', lines[i]):
+            # Save the input coordinates for this piece of the scan
             # Eat the header
             i += 5
             # Read the coordinates
@@ -40,9 +51,10 @@ def parse_waterint_log(filename):
             while lines[i][1] != '-':
                 (idx, atomic_num, atomic_type, x, y, z) = lines[i].split()
                 x, y, z = float(x), float(y), float(z)
-                current_frame_coords.append((int(atomic_num), x, y, z))
+                # Ignore dummy atoms which Gaussian says have atomic number -1
+                if int(atomic_num) > 0:
+                    current_frame_coords.append((int(atomic_num), x, y, z))
                 i += 1
-            coords.append(current_frame_coords)
 
         # Look for the calculated QM energies
         scfdone_match = re.search(r'SCF Done:(.+)', lines[i])
@@ -57,12 +69,18 @@ def parse_waterint_log(filename):
 
         scf = scf * HARTREES_PER_KCAL * THINGS_PER_MOLE
         if scf != 0:
+            # Keep the latest coordinate set for which there is an energy
+            coords.append(current_frame_coords)
+            if optimized is False:
+                current_optimized_params = {}
+            optimized_params.append(current_optimized_params)
             energies.append(scf)
+            optimized = False
             # print '%s: Energy: %f (relative: %f)' % (filename, scf, scf - energies[0])
 
         i += 1
-    
-    return energies, coords
+    print >>sys.stderr, 'Found %d energies and %d coord sets in %s' % (len(energies), len(coords), filename)
+    return energies, coords, optimized_params
 
 
 def load_psf(filename):
@@ -80,7 +98,7 @@ def load_psf(filename):
 
     # !NATOM block contains, for each atom, its type and charge, as well as residue assignment
     (num_atoms, kind) = get_psf_block_header(psf)
-    print "Number of atoms: %d" % num_atoms
+    print >>sys.stderr, "Number of atoms: %d" % num_atoms
     residue_map, charges, atom_types = [], [], []
 
     # Parse each atom line
@@ -112,7 +130,7 @@ def load_psf(filename):
         if found_end_of_solute is False and resname == "TIP3":
             found_end_of_solute = True
             num_solute_residues = num_residues - 1 # We subtract 1 because the residue count has incremented into the first non-solute atom
-            print "Looks like you have %d atoms and %d residues in the solute (guessed by taking the atoms before the first TIP3)." % (num_solute_atoms, num_solute_residues)
+            print >>sys.stderr, "Looks like you have %d atoms and %d residues in the solute (guessed by taking the atoms before the first TIP3)." % (num_solute_atoms, num_solute_residues)
             
         if found_end_of_solute is False:
             num_solute_atoms += 1
@@ -120,7 +138,7 @@ def load_psf(filename):
             residue_map.append(num_residues - 1)
 
     psf.readline() # Blank line
-    print "%s contains %d residues as far as I can tell" % (filename, num_residues)
+    print >>sys.stderr, "%s contains %d residues as far as I can tell" % (filename, num_residues)
 
     if found_end_of_solute is False:
         num_solute_residues = num_residues
@@ -140,7 +158,7 @@ def load_prm(filename):
     while l[0:4] != "NONB":
         l = prm.readline()
         if l == '':
-            print 'Seems to be no NONB block in %s. That is probably OK.' % filename
+            print >>sys.stderr, 'load_prm: Seems to be no NONB block in %s. That is probably OK.' % filename
             return params
         l = l.strip()
     if l[-1] == '-': l = prm.readline().strip()  # Eat extra nonsense
@@ -157,6 +175,8 @@ def load_prm(filename):
             continue
         l = nuke_comment_re.sub('', l).strip()
         if l == "": continue
+        # Less than 4 tokens means this is probably the end of the section
+        if len(l.split()) < 4: break
         if l[0:4] == "HBON" or l[0:4] == "NBFI": break
         # If the line doesn't start with a space, it's a new section, and we should stop
         # TODO: What's NBFIX?
@@ -207,7 +227,7 @@ def move_water(coords, offset=-0.2):
                     best_ligand_atom = ligand_atom
                     best_water_atom = water_atom
 
-    print 'move_water: Closest distance is %.3f, between atoms %d and %d' % (best_dist, best_ligand_atom, best_water_atom)
+    # print 'move_water: Closest distance is %.3f, between atoms %d and %d' % (best_dist, best_ligand_atom, best_water_atom)
 
     # Determine a vector of norm == offset and pointing toward the closest ligand atom
     _, lx, ly, lz = coords[best_ligand_atom]
@@ -278,7 +298,7 @@ def main():
     # Default scale value taken from isoflurane paper and its reasoning about discrepancy between
     # QM B3LYP water dimer interaction energy and MM TIP3P water dimer interaction energy
     ap.add_argument('--scale', type=float, default=-6.95/-4.84, help='Multiply QM energies by this number before printing them')
-    ap.add_argument('--shift', type=float, default=-0.0, help='Change the distance of the TIP3P water from ligand by this many Angstroms before calculating MM energy')
+    ap.add_argument('--shift', type=float, default=0.0, help='Change the distance of the TIP3P water from ligand by this many Angstroms before calculating MM energy')
     ap.add_argument('system_yaml')
     ap.add_argument('waterint_logs', nargs='+')
     args = ap.parse_args()
@@ -286,40 +306,49 @@ def main():
 
     ff_prm = {}
     for filename in system['prms']:
-        print 'Loading force field parameter file %s...' % filename
+        print >>sys.stderr, 'Loading force field parameter file %s...' % filename
         ff_prm.update(load_prm(filename))
 
-    print 'After loading all force field parameter files, I know about %d atomtypes.' % len(ff_prm.keys())
+    print >>sys.stderr, 'After loading all force field parameter files, I know about %d atomtypes.' % len(ff_prm.keys())
 
-    print 'Loading ligand PSF %s...' % system['psf']
+    print >>sys.stderr, 'Loading ligand PSF %s...' % system['psf']
     ligand_psf = load_psf(system['psf'])
 
     # We use single-point energies for the compound in question as well as water
     # as a baseline. For some reason FFTK only uses Hartree-Fock.
     # Will uncomment two lines below when we have some B3LYP energies
-    sp_energies, _ = parse_waterint_log(system['sp_ligand_log'])
-    sp_wat_energies, _ = parse_waterint_log(system['sp_wat_log'])
+    sp_energies, _, _ = parse_waterint_log(system['sp_ligand_log'])
+    sp_wat_energies, _, _ = parse_waterint_log(system['sp_wat_log'])
 
     # Extract the final QM energies from the water interaction calculations.
-    print 'Scale factor for QM energies: %.4f.' % args.scale
-    print 'Moving MM water by this much: %.2f Angstroms (negative means closer to ligand).' % args.shift
-    print ''
-    print 'Interaction energies - single TIP3P vs ligand:'
-    energies = {}
-    for filename in args.waterint_logs:
-        scf_energies, qm_coords = parse_waterint_log(filename)
-        energies[filename] = {}
-        #energies[filename]['qm'] = scf_energies[-1] - sp_hf_energies[-1] - sp_wat_energies[-1]
-        energies[filename]['qm'] = scf_energies[-1] - sp_energies[-1] - sp_wat_energies[-1]
-        energies[filename]['qm'] *= args.scale
-        energies[filename]['electro'], energies[filename]['lj'] = calc_mm_interaction_energy(qm_coords[-1], args.shift, ligand_psf, ff_prm)
+    print >>sys.stderr, 'Scale factor for QM energies: %.4f.' % args.scale
+    print >>sys.stderr, 'Moving MM water by this much: %.2f Angstroms (negative means closer to ligand).' % args.shift
+    print >>sys.stderr, ''
+    print >>sys.stderr, 'Interaction energies - single TIP3P vs ligand:'
 
-        # Present the energies to the user, for their perusal
-        print '%s: QM = %.3f, MMelec = %.3f, MMlj = %.3f, MMtot = %.3f' % (filename, energies[filename]['qm'],
-                                            energies[filename]['electro'],
-                                            energies[filename]['lj'],
-                                            energies[filename]['electro'] + energies[filename]['lj'])
-    
+    out = None
+
+    for filename in args.waterint_logs:
+        scf_energies, qm_coords, qm_params = parse_waterint_log(filename)
+
+        for frame_i in range(len(qm_coords)):
+            # Only show optimized energies
+            if qm_params[frame_i] == {}:
+                continue
+            # Don't fire up the CSV writer until we need it, because we don't know all the field names until now
+            if out is None:
+                fieldnames = ['filename', 'qm_energy', 'mm_electro', 'mm_lj', 'mm_energy']
+                fieldnames.extend(qm_params[frame_i].keys())
+                out = csv.DictWriter(sys.stdout, fieldnames=fieldnames)
+                out.writeheader()
+
+            data = qm_params[frame_i]
+            data['filename'] = filename
+            data['qm_energy'] = args.scale * (scf_energies[frame_i] - sp_energies[-1] - sp_wat_energies[-1])
+            data['mm_electro'], data['mm_lj'] = calc_mm_interaction_energy(qm_coords[frame_i], args.shift, ligand_psf, ff_prm)
+            data['mm_energy'] = data['mm_electro'] + data['mm_lj']
+            out.writerow(data)
+
 
 if __name__ == '__main__':
     sys.exit(int(main() or 0))
