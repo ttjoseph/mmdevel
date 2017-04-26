21b8ab6dda32834fb95b6b2ccd1ceeccfea994db
Tom Joseph <ttjoseph@gmail.com>
2017-04-26 15:01:13 -0400

    Add Gaussian SLURM submit script

---
+++ submit-g09.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Conveniently submit a Gaussian job to the friendly local SLURM.
+# Usage: submit-g09.sh whatever.gau
+nproc=`grep -i nproc $1 | cut -d= -f2`
+sbatch <<FOO
+#!/bin/bash
+#SBATCH -J $1
+#SBATCH -n $nproc
+
+g09 $1
+echo "Done with $1" | ~/bin/slack-ttjoseph
+FOO
