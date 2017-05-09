5352af6b98f9a1736dfd0d5474462382a2afae35
Tom Joseph <ttjoseph@gmail.com>
2017-05-09 10:19:36 -0400

    Teach calc_fepout_deltag.sh to make Gnuplot file also

--- calc_fepout_deltag.sh
+++ calc_fepout_deltag.sh
@@ -5,3 +5,7 @@ package require parsefep
 parsefep -gc 0 -forward $1
 quit
 FOO
+
+grep FepEnergy $1 | awk '{print $10}' > $1.fepenergy.gnuplot
+
+echo "Generated Gnuplot plot in $1.fepenergy.gnuplot"
