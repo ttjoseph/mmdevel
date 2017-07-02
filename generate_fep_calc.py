49ac268c820ccbbe474db9a75aed1d663d811058
Thomas Joseph <ttjoseph@gmail.com>
2017-07-02 12:08:50 -0400

    Allow lambdas to be denser near 0. More descripting argument names.

--- generate_fep_calc.py
+++ generate_fep_calc.py
@@ -10,9 +10,9 @@ from os.path import isfile
 
 sbatch_preambles = {}
 sbatch_preambles['stampede'] = """#!/bin/bash
-#SBATCH -J %(basename)s_%(group_id)d
-#SBATCH -o %(basename)s_%(group_id)d.out
-#SBATCH -e %(basename)s_%(group_id)d.err
+#SBATCH -J %(basenamdconf)s_%(group_id)d
+#SBATCH -o %(basenamdconf)s_%(group_id)d.out
+#SBATCH -e %(basenamdconf)s_%(group_id)d.err
 #SBATCH -p normal         # queue
 #SBATCH -N 4              # Number of nodes, not cores (16 cores/node)
 #SBATCH -n 64             # Total number of MPI tasks (if omitted, n=N)
@@ -29,9 +29,9 @@ NAMD=~/bin/namd-2.12
 """
 
 sbatch_preambles['stampede-knl'] = """#!/bin/bash
-#SBATCH -J %(basename)s_%(group_id)d
-#SBATCH -o %(basename)s_%(group_id)d.out
-#SBATCH -e %(basename)s_%(group_id)d.err
+#SBATCH -J %(basenamdconf)s_%(group_id)d
+#SBATCH -o %(basenamdconf)s_%(group_id)d.out
+#SBATCH -e %(basenamdconf)s_%(group_id)d.err
 #SBATCH -p normal         # queue
 #SBATCH -N 2              # Number of nodes, not cores (68 real cores per node)
 #SBATCH -n 8              # Total number of MPI tasks (if omitted, n=N)
@@ -49,9 +49,9 @@ NAMD="$TACC_NAMD_BIN/namd2 +ppn 32 +pemap 0-63+68 +commap 64-67"
 """
 
 sbatch_preambles['perceval'] = """#!/bin/bash
-#SBATCH -J %(basename)s_%(group_id)d
-#SBATCH -o %(basename)s_%(group_id)d.out
-#SBATCH -e %(basename)s_%(group_id)d.err
+#SBATCH -J %(basenamdconf)s_%(group_id)d
+#SBATCH -o %(basenamdconf)s_%(group_id)d.out
+#SBATCH -e %(basenamdconf)s_%(group_id)d.err
 #SBATCH -p main
 #SBATCH -x memnode001 # Don't use this node as it is real slow
 #SBATCH -N 1 -n 24
@@ -66,27 +66,31 @@ sbatch_preambles['perceval'] = """#!/bin/bash
 SRUN="srun -n $SLURM_NTASKS --mpi=pmi2"
 NAMD=$HOME/bin/namd2-2.12
 
-echo "Starting %(basename)s_%(group_id)d" | ~/bin/slack-ttjoseph
+echo "Starting %(basenamdconf)s_%(group_id)d" | ~/bin/slack-ttjoseph
 
 """
 
 
 
 ap = argparse.ArgumentParser(description='Generate mildly embarrasingly parallel FEP input for NAMD')
-ap.add_argument('basename', help='Base filename, e.g. "fep", such that we generate fep<n>.namd which sources common_fep.namd')
-ap.add_argument('inputname', help='Per-group input system name (e.g. equilibrated system)')
+ap.add_argument('basenamdconf', help='Base filename, e.g. "fep", such that we generate fep<n>.namd which sources common_fep.namd')
+ap.add_argument('startsystem', help='Per-group input system name (e.g. equilibrated system)')
 ap.add_argument('clustername', help='Supercomputer cluster you are using', default='perceval')
 ap.add_argument('--num-windows', type=int, default=30, help='Number of lambda windows')
+ap.add_argument('--left-dense', dest='left_dense', action='store_true', help='Denser lambda coverage closer to 0, rather than 1?')
 ap.add_argument('--windows-per-group', type=int, default=5, help='Maximum number of lambda windows per batch job')
 ap.add_argument('--per-group-num-equil-steps', type=int, default=500000)
 ap.add_argument('--per-window-num-equil-steps', type=int, default=200000)
 ap.add_argument('--per-window-prod-steps', type=int, default=1000000)
-ap.add_argument('--continue', dest='continue_prod', action='store_true', help='Continue from previous run as specified by inputname')
-ap.set_defaults(continue_prod=False)
+ap.add_argument('--continue', dest='continue_prod', action='store_true', help='Continue from previous run as specified by startsystem')
+ap.set_defaults(continue_prod=False, left_dense=False)
 args = ap.parse_args()
 
 x = np.linspace(0, 1.0, num=(args.num_windows+1), endpoint=True)
-all_lambdas = np.sin((np.pi/2)*x)
+# Make the lambdas denser to one side or the other of the FEP run.
+# Often, there will be big dG changes with low or high lambda values, so this is intended
+# to sample those areas more by using smaller windows.
+all_lambdas = np.sin((np.pi/2)*x) if args.left_dense is False else (1-np.cos((np.pi/2)*x))
 print('Here are the %d lambda windows:' % args.num_windows)
 print(all_lambdas)
 group_id = 0
@@ -97,9 +101,9 @@ for i in range(len(all_lambdas)-1):
         group_id += 1
 
     data = {
-        'basename': args.basename,
-        'inputname': args.inputname,
-        'outputname': '%s%03d' % (args.basename, i),
+        'basenamdconf': args.basenamdconf,
+        'startsystem': args.startsystem,
+        'outputname': '%s%03d' % (args.basenamdconf, i),
         'group_id': group_id,
         'l0': all_lambdas[i],
         'l1': all_lambdas[i+1]
@@ -107,22 +111,22 @@ for i in range(len(all_lambdas)-1):
     if starting_a_group:
         data['alchequilsteps'] = args.per_group_num_equil_steps
     else:
-        data['inputname'] = '%s%03d' % (args.basename, i - 1)
+        data['startsystem'] = '%s%03d' % (args.basenamdconf, i - 1)
         data['alchequilsteps'] = args.per_window_num_equil_steps
 
     # If we are adding on more production sampling to a previous run, we don't need to
     # do any equilibration, and we should start from the same window from the previous run
     if args.continue_prod is True:
         data['alchequilsteps'] = 0
-        data['inputname'] = '%s%03d' % (args.inputname, i)
+        data['startsystem'] = '%s%03d' % (args.startsystem, i)
 
     data['totalsteps'] = data['alchequilsteps'] + args.per_window_prod_steps
     s = """# Do FEP for lambda %(l0)f to %(l1)f
-set inputname %(inputname)s;
+set inputname %(startsystem)s;
 set outputname %(outputname)s;
 alchLambda %(l0)f
 alchLambda2 %(l1)f
-source common_%(basename)s.namd
+source common_%(basenamdconf)s.namd
 alchEquilSteps %(alchequilsteps)d
 run %(totalsteps)d
 """ % data
@@ -132,8 +136,8 @@ run %(totalsteps)d
     # Make a submit script for this window group, for convenience
     if starting_a_group:
         s = sbatch_preambles[args.clustername] % data
-        with open('submit-%s-group-%d.sh' % (args.basename, group_id), 'w') as f:
+        with open('submit-%s-group-%d.sh' % (args.basenamdconf, group_id), 'w') as f:
             f.write(s)
             for j in range(i, i+args.windows_per_group):
-                f.write('$SRUN $NAMD %s%03d.namd > %s%03d.log\n' % (args.basename, j, args.basename, j))
-                f.write('echo "%s%03d (group %d) done" | ~/bin/slack-ttjoseph\n\n' % (args.basename, j, group_id))
+                f.write('$SRUN $NAMD %s%03d.namd > %s%03d.log\n' % (args.basenamdconf, j, args.basenamdconf, j))
+                f.write('echo "%s%03d (group %d) done" | ~/bin/slack-ttjoseph\n\n' % (args.basenamdconf, j, group_id))
