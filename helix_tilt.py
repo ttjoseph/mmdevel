f1d99bd744f0160b6cee59af81211a293822c461
Tom Joseph <ttjoseph@gmail.com>
2017-11-16 15:04:53 -0500

    Bake in some GPCR residue ranges, emit a Gnuplot script

--- helix_tilt.py
+++ helix_tilt.py
@@ -3,6 +3,16 @@ import argparse
 import sys
 import numpy as np
 import MDAnalysis as mda
+import matplotlib.pyplot as plt
+
+# Convenient resid ranges for the transmembrane helices of selected proteins
+# 5TVN: 5HT2B receptor
+# 5C1M: MuOR
+# 4DJH: KappaOR
+HELIX_LISTS = {'5tvn': '50-80,90-117,127-159,169-193,215-245,321-350,356-382',
+               '5c1m': '65-98,102-130,137-169,181-205,226-259,270-304,313-343',
+               '4djh': '56-86,93-121,128-161,171-196,219-255,270-299,310-332'}
+
 
 def closest_to_z_axis(*vectors):
     """Of the vectors supplied, returns the one that has the smallest angle with respect to the Z axis."""
@@ -26,7 +36,7 @@ def angle_in_degrees(u, v):
 
 if __name__ == '__main__':
     ap = argparse.ArgumentParser(description='Calculate helix tilts over a trajectory, relative to longest protein axis')
-    ap.add_argument('helixlist', help='Helix residue ID ranges, inclusive, separated by commas with no whitespace; e.g. 10-20,60-90' )
+    ap.add_argument('helixlist', help='Helix residue ID ranges, inclusive, separated by commas with no whitespace; e.g. 10-20,60-90. Or a list alias.' )
     ap.add_argument('psf', help='PSF topology file')
     ap.add_argument('dcd', nargs='+', help='DCD trajectory file[s]')
     args = ap.parse_args()
@@ -34,14 +44,15 @@ if __name__ == '__main__':
     u = mda.Universe(args.psf, args.dcd)
 
     # Parse the helix residue ID ranges
+    helixlist = HELIX_LISTS[args.helixlist] if args.helixlist in HELIX_LISTS else args.helixlist
     helices, labels = [], []
-    for helix in args.helixlist.split(','):
+    for helix in helixlist.split(','):
         resids = [int(x) for x in helix.split('-')]
         if len(resids) != 2:
             raise ValueError('Your helix specification %s does not have exactly two residue IDs' % helix)
         helices.append(u.select_atoms('protein and name CA and resid %d:%d' % (resids[0], resids[1])))
-        labels.append('"Radial %d-%d"' % (resids[0], resids[1]))
-        labels.append('"Lateral %d-%d"' % (resids[0], resids[1]))
+        labels.append('"Rad%d-%d"' % (resids[0], resids[1]))
+        labels.append('"Lat%d-%d"' % (resids[0], resids[1]))
     # Print CSV label
     print ','.join(labels)
 
@@ -71,16 +82,24 @@ if __name__ == '__main__':
             vals.append(radial_tilting_angle)
             vals.append(lateral_tilting_angle)
 
-            # Print out diagnostic VMD arrow drawing commands if we see a really wacky lateral tilting angle
-            # if lateral_tilting_angle > 20:
-            #     logging.info("Frame %d, radial=%.2f, lateral=%.2f" % (frame_num, radial_tilting_angle,
-            #                                                           lateral_tilting_angle))
-            #     print "graphics top color blue"
-            #     vmd_draw_arrow(channel.centerOfMass(), c1*20)
-            #     vmd_draw_arrow(channel.centerOfMass(), radial_plane_normal)
-            #     print "graphics top color red"
-            #     vmd_draw_arrow(tm.centerOfMass(), a1*20)
-            #     exit()
-
         print ','.join(['%.2f' % x for x in vals])
 
+
+    # plt.figure()
+    # plt.savefig('helices.png', bbox_inches='tight')
+    print >>sys.stderr, """# Gnuplot script to plot this data
+set datafile separator ','
+fname = 'foo.csv'
+set xlabel 'Radial tilt (degrees)'
+set ylabel 'Lateral tilt (degrees)'
+set xrange [-20:40]
+set yrange [-3 0:50]
+set term png
+set output 'foo.png'
+"""
+    s = []
+    for i in range(0, len(labels), 2):
+        # Using two columns at a time
+        s.append("fname using %d:%d title 'TM%d'" % (i+1, i+2, i/2+1))
+
+    print >>sys.stderr, 'plot %s' % ', '.join(s)
