d5d65a8c00215c9433e957fb4a91e0ade831f5e6
Tom Joseph <ttjoseph@gmail.com>
2017-12-23 22:09:21 -0600

    Add script to calculate angles among atoms over time.
    I guess you could use VMD. Oh well.

---
+++ angle_over_traj.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import argparse
+import sys
+import numpy as np
+import MDAnalysis as mda
+
+def angle_in_degrees(u, v):
+    """Returns the angle between two vectors, in degrees."""
+    angle = np.arccos(np.dot(u, v)/np.linalg.norm(u)/np.linalg.norm(v))
+    return angle*(180.0/np.pi)
+
+
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser(description='Calculate angle formed by three residues over a trajectory')
+    ap.add_argument('groups', help='Residue IDs, prefixed by "SEGID:", separated by hyphens, groups separated by commas, and no whitespace: e.g. "PROA:a-b-c" for the a-b-c angle.' )
+    ap.add_argument('psf', help='PSF topology file')
+    ap.add_argument('dcd', nargs='+', help='DCD trajectory file[s]')
+    args = ap.parse_args()
+
+    print >>sys.stderr, 'Loading %s and %s...' % (args.psf, args.dcd)
+    u = mda.Universe(args.psf, args.dcd)
+
+    # Parse resid group list
+    group_strs = args.groups.split(',')
+    groups, segids = [], []
+    for s in group_strs:
+        segid, resid_str = s.split(':')
+        segids.append(segid)
+        resids = [int(a) for a in resid_str.split('-')]
+        group = []
+        for resid in resids:
+            atoms = u.select_atoms('protein and segid %s and resid %d and name CA' % (segid, resid))
+            group.append(atoms[0])
+        groups.append(group)
+
+    # Convenient CSV header was provided by user
+    print args.groups
+
+    # Iterate through trajectory frames and calculate those angles
+    for ts in u.trajectory:
+        vals = []
+        for group in groups:
+            vals.append(angle_in_degrees(group[2].position - group[1].position, group[1].position - group[0].position))
+        print ','.join(['%.2f' % v for v in vals])
