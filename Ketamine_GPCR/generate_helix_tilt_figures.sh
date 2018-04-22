45aa9dca6ac2d554dbe3d397125373880e08d9ff
Tom Joseph <ttjoseph@gmail.com>
2018-04-22 13:28:52 -0400

    Helix tilts: Calculate absolute tilts

--- Ketamine_GPCR/generate_helix_tilt_figures.sh
+++ Ketamine_GPCR/generate_helix_tilt_figures.sh
@@ -22,15 +22,26 @@ for sim in $SIMULATIONS
 do
     helixes=$(echo $sim | cut -d: -f1)
     dir=$(echo $sim | cut -d: -f2)
+    outname=$(echo $dir | sed s#/#-#)
     cd $B/$dir/namd || exit
     echo Working on: $sim prod*.dcd
+
+    # Perhaps the best strategy here is to calculate absolute helix tilts, then calculate differences
+    # outside helix_tilt.py. Perhaps in that Excel spreadsheet. Otherwise, we'd have to do unholy
+    # merging of structure/coordinates in order to get things to work.
+    # We can calculate the helix tilts for 4DKL (and the analogous KOR structure) once, and postprocess.
+
     gnuplot_script=`mktemp`
-    python $MMDEVEL/helix_tilt.py $helixes ../step5_assembly.xplor_ext.psf ../step5_assembly.namd.pdb prod*.dcd > $gnuplot_script
+    python $MMDEVEL/helix_tilt.py --absolute $helixes ../step5_assembly.xplor_ext.psf ../step5_assembly.namd.pdb prod*.dcd > $gnuplot_script
     # gnuplot $gnuplot_script
+    python $MMDEVEL/csv_stats.py helix-tilt-absolute.csv > $HOME/Dropbox/Ketamine_GPCR_Paper/HelixTilt/${outname}-absolute.csv
+
+    #python $MMDEVEL/helix_tilt.py $helixes ../step5_assembly.xplor_ext.psf ../step5_assembly.namd.pdb prod*.dcd > $gnuplot_script
+    # gnuplot $gnuplot_script
+    #python $MMDEVEL/csv_stats.py helix-tilt.csv > $HOME/Dropbox/Ketamine_GPCR_Paper/HelixTilt/${outname}.csv
+
     rm -f $gnuplot_script
 
-    outname=$(echo $dir | sed s#/#-#)
-    python $MMDEVEL/csv_stats.py helix-tilt.csv > $HOME/Dropbox/Ketamine_GPCR_Paper/HelixTilt/${outname}.csv
 done
 
 cd $HERE
--- helix_tilt.py
+++ helix_tilt.py
@@ -7,8 +7,8 @@ import matplotlib.pyplot as plt
 
 # Convenient resid ranges for the transmembrane helices of selected proteins
 # 5TVN: 5HT2B receptor
-# 5C1M: MuOR
-# 4DJH: KappaOR
+# 5C1M: MuOR (active form; inactive from is 4DKL)
+# 4DJH: KappaOR with inhibitor JDTic (active form is 6B73)
 HELIX_LISTS = {'5tvn': '50-80,90-117,127-159,169-193,215-245,321-350,356-382',
                '5c1m': '65-98,102-130,137-169,181-205,226-259,270-304,313-343',
                '4djh': '56-86,93-121,128-161,171-196,219-255,270-299,310-332'}
@@ -21,7 +21,10 @@ def closest_to_z_axis(*vectors):
     min_vector = None
     for v in vectors:
         angle = angle_in_degrees(v, z_axis)
-        # TODO: This doesn't work because we should consider positive and negative vectors that are parallel the same
+        # If this vector is "upside down" with respect to the z-axis, flip the angle.
+        # For example, if the angle is 170 degrees, we really meant 10 degrees
+        if angle > 90:
+            angle = 180 - angle
         if np.abs(angle) < min_angle:
             min_vector = v
             min_angle = angle
@@ -43,7 +46,8 @@ if __name__ == '__main__':
     args = ap.parse_args()
 
     u = mda.Universe(args.psf, args.dcd)
-    out_file = open('helix-tilt.csv', 'w')
+    out_file_name = 'helix-tilt.csv' if args.absolute is False else 'helix-tilt-absolute.csv'
+    out_file = open(out_file_name, 'w')
 
     # Parse the helix residue ID ranges
     helixlist = HELIX_LISTS[args.helixlist] if args.helixlist in HELIX_LISTS else args.helixlist
@@ -64,28 +68,32 @@ if __name__ == '__main__':
     # Iterate over frames in the trajectory
     protein = u.select_atoms('protein and name CA')
     for ts in u.trajectory:
-        c3, c2, c1 = protein.principal_axes()
+        c1, c2, c3 = protein.principal_axes()
+        c1 = closest_to_z_axis(c1, c2, c3)
         # Force the major axis to always point up. Normally this should be the z-axis
         # print >>sys.stderr, c1
         if c1[2] < 0: c1 = -c1
-        # Assert that we've chosen the pricipal axis closest to the Z axis
-        assert(c1[2] > c1[0])
-        assert(c1[2] > c1[1])
+        # Assert that we've chosen the principal axis closest to the Z axis
+        #assert(c1[2] > c1[0])
+        #assert(c1[2] > c1[1])
 
         # Calculate angles between the various principal axes.
         # The vector in the xy plane originating at the channel COM and extending to the COM of the TM2 vector
         # is normal to the plane against which we measure the TM2 tilting angle for radial tilt.
         vals = []
         for helix in helices:
-            a3, a2, a1 = helix.principal_axes()
+            a1, a2, a3 = helix.principal_axes()
+            a1 = closest_to_z_axis(a1, a2, a3)
             # Force the first axis to point up, always
             if a1[2] < 0: a1 = -a1
-            # Assert that we've chosen the pricipal axis closest to the Z axis
-            assert (a1[2] > a1[0])
-            assert (a1[2] > a1[1])
+            # Assert that we've chosen the principal axis closest to the Z axis
+            # (Which as it turns out not a safe assumption for individual helices)
+            #assert a1[2] > a1[0], 'z-axis not the biggest one: %s' % (str(a1))
+            #assert a1[2] > a1[1], 'z-axis not the biggest one: %s' % (str(a1))
             radial_plane_normal = helix.center_of_mass() - protein.center_of_mass()
             # print >>sys.stderr, radial_plane_normal[2]
-            # radial_plane_normal[2] = 0
+            # Force it to be on a plane orthogonal to the global Z axis
+            radial_plane_normal[2] = 0
             radial_tilting_angle = 90-angle_in_degrees(a1, radial_plane_normal)
             if first_radial_tilting_angle[helix] is None:
                 first_radial_tilting_angle[helix] = radial_tilting_angle
