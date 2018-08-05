3467d51ea4be211d750f0c7c5c1a6e68baa6930d
Tom Joseph <ttjoseph@gmail.com>
2018-08-05 17:30:17 -0400

    Add script to find and generate harmonic restraints for missing loops

---
+++ generate_protein_gap_restraints.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+#
+# Finds gaps in PDB files, which you probably want to fill in with a loop modeler or something
+import argparse
+import MDAnalysis as mda
+from scipy.spatial.distance import euclidean
+
+ap = argparse.ArgumentParser(description='Find missing loops in a protein structure')
+ap.add_argument('pdb', help='Protein structure')
+args = ap.parse_args()
+
+u = mda.Universe(args.pdb)
+protein = u.select_atoms('protein')
+
+last_res = None
+
+for res in protein.residues:
+    if last_res is None:
+        last_res = res
+
+    # Mod 10000 because resid can only be four decimal digits
+    if res.segid == last_res.segid and res.resid > last_res.resid + 1:
+        print ('# Discontinuity in segid %s between residues %d and %d (length %d)' \
+               % (res.segid, last_res.resid % 10000, res.resid % 10000, res.resid - last_res.resid - 1))
+        a1 = res.atoms.select_atoms('name CA')[0]
+        a2 = last_res.atoms.select_atoms('name CA')[0]
+        distance = euclidean(a1.position, a2.position)
+        print('# Distance between CA: %f' % distance)
+
+        print("""colvar {
+    name gap_%s%d_%d
+    distance {
+        group1 { atomNumbers %d }
+        group2 { atomNumbers %d }
+    }
+}""" % (res.segid, res.resid, last_res.resid, a1.id, a2.id))
+        print("""harmonic {
+    colvars gap_%s%d_%d
+    centers %f
+    forceConstant 10.0
+}""" % (res.segid, res.resid, last_res.resid, distance))
+        
+    last_res = res
