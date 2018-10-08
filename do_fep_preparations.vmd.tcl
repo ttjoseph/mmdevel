33150d1810155e4d6b4eb14ea0cf77dadd873c8f
Tom Joseph <ttjoseph@gmail.com>
2018-10-08 11:01:04 -0400

    Make output more pasteable

--- do_fep_preparations.vmd.tcl
+++ do_fep_preparations.vmd.tcl
@@ -1,11 +1,11 @@
 # Helper stuff for generating the various files necessary for a FEP with spherical restraints
 
-# Assumes the top molecule is the system and we want to disappear $ligname
+# Assumes the top molecule is the system and we want to disappear $ligspec
 # and that the ligand is not a protein
-proc do_fep_preparations {ligname} {
+proc do_fep_preparations {ligspec} {
 	# Make disappear.fep
 	set all [atomselect top all]
-	set lig [atomselect top "resname $ligname"]
+	set lig [atomselect top "$ligspec"]
 
 	$all set beta 0
 	$all set occupancy 0
@@ -13,19 +13,22 @@ proc do_fep_preparations {ligname} {
 	# Tell NAMD which atoms to decouple
 	$lig set beta -1
 	$all writepdb disappear.fep
+	puts "# Wrote FEP atom selection disappear.fep"
 
 	# Tell colvars which atoms to use for reorientation
 	$all set beta 0
-	set restref [atomselect top "protein and backbone within 7 of resname $ligname"]
+	set restref [atomselect top "protein and backbone within 7 of ($ligspec)"]
 	set restref_len [llength [$restref list]]
-	puts "rest_ref uses $restref_len atoms. Make sure that is OK!"
+	puts "# rest_ref uses $restref_len atoms. Make sure that is OK!"
 	puts ""
 
 	$restref set occupancy 1
 	$all writepdb rest_ref.pdb
+	puts "# Wrote restraint reference rest_ref.pdb"
 	$all set occupancy 0
 	[atomselect top "protein and name CA"] set occupancy 1
 	$all writepdb dont_spin_ref.pdb
+	puts "# Wrote dont_spin_ref.pdb"
 
 	# Show ligand atomNumbers for the benefit of colvars
 	set nums {}
@@ -35,6 +38,7 @@ proc do_fep_preparations {ligname} {
 	puts "atomNumbers $nums"
 
 	# Show COM (COG?) of the ligand, again for colvars
-	puts "Center of ligand $ligname:"
-	puts [measure center $lig]
+	puts "# Center of ligand $ligspec:"
+	set lig_center [join [measure center $lig] ", "]
+	puts "dummyAtom ($lig_center)"
 }
