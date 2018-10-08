20a41839a767527e4ec6a76d5bdaf563811c66ae
Tom Joseph <ttjoseph@gmail.com>
2018-10-08 10:59:23 -0400

    Add script to submit Gaussian16 job to SLURM

---
+++ submit-g16.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Conveniently submit a Gaussian job to the friendly local SLURM.
+# Usage: submit-g16.sh whatever.gau
+nproc=`grep -i nproc $1 | cut -d= -f2`
+sbatch <<FOO
+#!/bin/bash
+#SBATCH -J $1
+#SBATCH -n $nproc
+
+g16 $1
+echo "Done with $1 in \$PWD" | ~/bin/slack-ttjoseph
+FOO
