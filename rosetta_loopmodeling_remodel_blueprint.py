bf9097f8f37857e1f335c735c2a2a6911fc5aced
Tom Joseph <ttjoseph@gmail.com>
2018-11-15 17:06:08 -0500

    Add script to generate Rosetta Remodel input for ab initio loop generation

---
+++ rosetta_loopmodeling_remodel_blueprint.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+#
+# Generates a blueprint file to be used as input to the Rosetta 3 "remodel" tool,
+# so that it can generate missing loops, most likely with an ab initio protocol.
+
+import argparse
+import sys
+import os
+import MDAnalysis as mda
+from Bio import SeqIO
+
+def main():
+    ap = argparse.ArgumentParser(description='Generate Rosetta remodel blueprint file for ab initio loop modeling')
+    ap.add_argument('pdb', help='PDB file of your protein with missing loops')
+    ap.add_argument('segid', help='Which segid you want. Sorry, only one is allowed')
+    ap.add_argument('fasta', help='FASTA sequence file of the specified segid')
+    ap.add_argument('-l', '--loops', help='Care only about loops starting at these comma-separated resids (default: all)')
+    ap.add_argument('-m', '--max-loop-length', type=int, default=50, help='Maximum length of loop to fill')
+    args = ap.parse_args()
+
+    only_these_loops = [int(x) for x in args.loops.split(',')] if args.loops is not None else None
+
+    u = mda.Universe(args.pdb)
+    protein = u.select_atoms('protein and segid %s' % args.segid)
+    sequence = SeqIO.read(args.fasta, 'fasta').seq
+
+    output_dirname = 'output_%s_%s' % (args.pdb, args.segid)
+    if not os.path.exists(output_dirname):
+        os.mkdir(output_dirname)
+    os.chdir(output_dirname)
+
+    # First determine where all the loops are
+    _, all_loop_starts, all_loop_lengths = gen_blueprint(protein, sequence)
+
+    print >>sys.stderr, '##### Going back and generating input files for Rosetta remodel #####'
+
+    # Now write individual blueprint and conf files for each loop
+    # We do each loop separately so we can parallelize. It will be a little annoying to combine all the loops
+    # back again but we can probably write a script to do it.
+    for i in range(len(all_loop_starts)):
+        loop_start, loop_length = all_loop_starts[i], all_loop_lengths[i]
+
+        if loop_length <= args.max_loop_length:
+            blueprint_entries, loop_starts, loop_lengths = gen_blueprint(protein, sequence, [loop_start,])
+            prefix = '%s.%s.%d_%d' % (args.pdb, args.segid, loop_start, loop_lengths[0])
+            with open('%s.blueprint' % prefix, 'w') as f:
+                print >>f, '\n'.join(blueprint_entries)
+            with open('%s.remodel.conf' % prefix, 'w') as f:
+                print >>f, """-in:file:s ../%s
+-remodel:blueprint %s.blueprint
+
+-run:chain %s
+-remodel:num_trajectory 20 
+-nstruct 500
+
+-out:path:all .
+-out:file:scorefile %s.sc
+""" % (args.pdb, prefix, args.segid, prefix)
+
+
+def gen_blueprint(protein, sequence, only_these_loops=None):
+    last_res = None
+    rosetta_resid = 1
+    blueprint_entries = []
+    loop_starts, loop_lengths = [], []
+
+    for res in protein.residues:
+        res_1let = mda.lib.util.convert_aa_code(res.resname)
+        res_seq = sequence[res.resid-1]
+        # Don't forget, resid counts starting at 1
+        if res_1let != res_seq:
+            print >>sys.stderr, 'Bad news: res_1let %s does not match sequence %s at %d. Is this the right FASTA?' % \
+                (res_1let, res_seq, res.resid)
+            exit(1)
+
+        if last_res is None:
+            last_res = res
+
+        discon_length = res.resid - last_res.resid - 1
+        if discon_length > 0 and (only_these_loops is None or last_res.resid in only_these_loops):
+            print >>sys.stderr, '# Discontinuity between residues %s:%s%d and %s:%s%d (length %d)' \
+                   % (last_res.segid, last_res.resname, last_res.resid % 10000,
+                      res.segid, last_res.resname, res.resid % 10000, discon_length)
+
+            # We need to include the residues on either end of the loop, for which we do know
+            # the coordinates, in the loop modeling, so as to allow sufficient flexibility
+            lastres_1let = mda.lib.util.convert_aa_code(last_res.resname)
+            # The last entry in the blueprint must be replaced
+            blueprint_entries.pop()
+            # Tell Rosetta remodel to pick the correct amino acid ("PIKAA" command) for this _L_oop
+            blueprint_entries.append('%d %s L PIKAA %s' % (rosetta_resid-1, lastres_1let, lastres_1let))
+            for i in range(last_res.resid+1, res.resid):
+                blueprint_entries.append('0 X L PIKAA %s' % sequence[i-1])
+            blueprint_entries.append('%d %s L PIKAA %s' % (rosetta_resid, res_1let, res_1let))
+            loop_starts.append(last_res.resid)
+            loop_lengths.append(discon_length)
+        else:
+            # Tell Rosetta remodel to do nothing ("." command)
+            blueprint_entries.append('%d %s .' % (rosetta_resid, res_1let))
+
+        last_res = res
+        rosetta_resid += 1
+
+    return blueprint_entries, loop_starts, loop_lengths
+
+
+if __name__ == '__main__':
+    main()
