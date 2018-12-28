f94e829b5ef492a7b6458f0479084aa0020ac1c2
Tom Joseph <ttjoseph@gmail.com>
2018-12-28 18:12:20 -0500

    Be a little more concise

--- rosetta_loopmodeling_remodel_blueprint.py
+++ rosetta_loopmodeling_remodel_blueprint.py
@@ -6,8 +6,52 @@
 import argparse
 import sys
 import os
+from shutil import copy2
 import MDAnalysis as mda
 from Bio import SeqIO
+
+class PDBEmitter(object):
+    """Stateful emitter for PDB (Protein Data Bank) formatted files.
+
+    Takes MDAnalysis atoms. Automatically renumbers ATOMs but does not change residue IDs.
+    This is intended to assist in merging two structures into one PDB file.
+
+    Example:
+        pdb = PDBEmitter(sys.stdout)
+        u1 = MDAnalysis.Universe('foo.pdb')
+        u2 = MDAnalysis.Universe('bar.pdb')
+        pdb.emit_atom(u2.atoms[13])
+        pdb.emit_atom(u1.atoms[42])
+    """
+
+    def __init__(self, out):
+        self.out = out
+        self.curr_atom_id = 1
+        self.last_emitted_resid = 0
+        self.last_encountered_resid = None
+
+    def emit_atom(self, atom, occupancy=0.0, resid=None, renumber_resid=False):
+        """Emits one ATOM record.
+
+        Returns emitted resid."""
+        if resid is None:
+            if renumber_resid is True:
+                resid = self.last_emitted_resid
+                if atom.residue.resid != self.last_encountered_resid:
+                    resid += 1
+            else:
+                resid = atom.residue.resid
+        # Format string from: http://cupnet.net/pdb-format/
+        self.out.write(
+            '{:6s}{:5d} {:^4s}{:1s}{:3s} {:1s}{:4d}{:1s}   {:8.3f}{:8.3f}{:8.3f}{:6.2f}{:6.2f}          {:>2s}{:2s}\n'.format(
+            "ATOM", self.curr_atom_id, atom.name, atom.altLoc, atom.residue.resname, atom.residue.segid, resid,
+                  ' ', atom.position[0], atom.position[1], atom.position[2],
+                  occupancy, 0.0, atom.type, '  ')) # occupancy, temp factor, type, charge
+        self.curr_atom_id += 1
+        self.last_emitted_resid = resid
+        self.last_encountered_resid = atom.residue.resid
+        return resid
+
 
 def main():
     ap = argparse.ArgumentParser(description='Generate Rosetta remodel blueprint file for ab initio loop modeling')
@@ -19,16 +63,31 @@ def main():
     ap.add_argument('-m', '--max-loop-length', type=int, default=50, help='Maximum length of loop to fill')
     args = ap.parse_args()
 
-    only_these_loops = [int(x) for x in args.loops.split(',')] if args.loops is not None else None
+    # only_these_loops = [int(x) for x in args.loops.split(',')] if args.loops is not None else None
 
     u = mda.Universe(args.pdb)
     protein = u.select_atoms('protein and segid %s' % args.segid)
     sequence = SeqIO.read(args.fasta, 'fasta').seq
 
-    output_dirname = args.output_dirname or 'remodel_%s_%s' % (args.pdb, args.segid)
+    output_dirname = os.path.abspath(args.output_dirname or 'remodel_%s_%s' % (args.pdb, args.segid))
     if not os.path.exists(output_dirname):
         os.mkdir(output_dirname)
+    copy2(args.pdb, output_dirname)
     os.chdir(output_dirname)
+
+    # Renumber PDB for Rosetta, because it will silently generate garbage unless resids start from
+    # 1 and increase without gaps
+    renumbered_protein_filename = 'renumbered-protein.pdb'
+    with open(renumbered_protein_filename, 'w') as f:
+        e = PDBEmitter(f)
+        real_to_rosetta_resid = dict()
+        for a in protein.atoms:
+            rosetta_resid = e.emit_atom(a, renumber_resid=True)
+            real_to_rosetta_resid[a.residue.resid] = rosetta_resid
+
+        with open('real-to-rosetta-resid.map', 'w') as f2:
+            for real_resid in sorted(real_to_rosetta_resid.keys()):
+                f2.write('%d %d\n' % (real_resid, real_to_rosetta_resid[real_resid]))
 
     # First determine where all the loops are
     _, all_loop_starts, all_loop_lengths = gen_blueprint(protein, sequence)
@@ -42,25 +101,28 @@ def main():
         loop_start, loop_length = all_loop_starts[i], all_loop_lengths[i]
 
         if loop_length <= args.max_loop_length:
+            os.chdir(output_dirname)
             blueprint_entries, loop_starts, loop_lengths = gen_blueprint(protein, sequence, [loop_start,])
             assert loop_start == loop_starts[0], 'Bug: confused about loop start'
             assert loop_length == loop_lengths[0], 'Bug: confused about loop length'
             this_output_dirname = 'loop%d_%d' % (loop_start, loop_length)
             if not os.path.exists(this_output_dirname):
                 os.mkdir(this_output_dirname)
-            prefix = '%s.%s.%d_%d' % (args.pdb, args.segid, loop_start, loop_length)
+            os.chdir(this_output_dirname)
+            prefix = 'remodel'
             with open('%s.blueprint' % prefix, 'w') as f:
                 print >>f, '\n'.join(blueprint_entries)
-            with open('%s.remodel.conf' % prefix, 'w') as f:
+            with open('%s.conf' % prefix, 'w') as f:
                 print >>f, """-in:file:s ../%s
+-ignore_zero_occupancy false
 -remodel:blueprint %s.blueprint
 -run:chain %s
 -remodel:num_trajectory 1
--nstruct 100
+-nstruct 500
 -out:level 100
--out:path:all %s
+-out:path:all .
 -out:file:scorefile %s.sc
-""" % (args.pdb, prefix, args.segid, this_output_dirname, prefix)
+""" % (renumbered_protein_filename, prefix, args.segid, prefix)
 
 
 # Generates blueprint file which tells Rosetta Remodel what to do with each residue in the protein
@@ -101,11 +163,17 @@ def gen_blueprint(protein, sequence, only_these_loops=None):
             for i in range(last_res.resid+1, res.resid):
                 blueprint_entries.append('0 X L PIKAA %s' % sequence[i-1])
             blueprint_entries.append('%d %s L PIKAA %s' % (rosetta_resid, res_1let, res_1let))
+
+            # blueprint_entries.append('%d %s L PIKAA %s' % (last_res.resid, lastres_1let, lastres_1let))
+            # for i in range(last_res.resid+1, res.resid):
+            #     blueprint_entries.append('0 X L PIKAA %s' % sequence[i-1])
+            # blueprint_entries.append('%d %s L PIKAA %s' % (res.resid, res_1let, res_1let))
             loop_starts.append(last_res.resid)
             loop_lengths.append(discon_length)
         else:
             # Tell Rosetta remodel to do nothing ("." command)
             blueprint_entries.append('%d %s .' % (rosetta_resid, res_1let))
+            # blueprint_entries.append('%d %s .' % (res.resid, res_1let))
 
         last_res = res
         rosetta_resid += 1
