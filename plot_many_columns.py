3e0a16b6a131833936a407cdcbea36daa54c5dd5
Tom Joseph <ttjoseph@gmail.com>
2019-08-31 15:24:02 -0400

    Allow better labeling and selection of individual columns

--- plot_many_columns.py
+++ plot_many_columns.py
@@ -11,16 +11,30 @@ if __name__ == '__main__':
     ap = argparse.ArgumentParser(description='Plot a CSV of time-series data')
     ap.add_argument('in_csv', help='Input CSV file')
     ap.add_argument('out_img', help='Output image file')
-    ap.add_argument('--x-label', help='X-axis label')
-    ap.add_argument('--y-label', help='Y-axis label')
+    ap.add_argument('--columns', help='Column indices to include, comma-separated, starting from 0 (default: all)')
+    ap.add_argument('--x-label', default='', help='X-axis label')
+    ap.add_argument('--y-label', default='', help='Y-axis label')
     ap.add_argument('--x-scale', type=float, default=1, help='Scale index of X axis data by this much (e.g. to convert trajectory frame number to time)')
+    ap.add_argument('--legend-labels', help='Comma-separated labels for the legend. Yes, that means you can\'t include a comma in a label')
     args = ap.parse_args()
 
     df = pd.read_csv(args.in_csv)
-    mpl.rcParams['lines.linewidth'] = 0.5
-    plt.figure(figsize=(7.5, 5))
+
+    if args.columns is not None:
+        cols = [int(x) for x in args.columns.split(',')]
+        df = df.iloc[:, cols]
+
+    mpl.rcParams['lines.linewidth'] = 0.25
+    
     df.plot(x=df.index*args.x_scale, y=df.columns)
     plt.xlabel(unicode(args.x_label, 'utf8'))
     plt.ylabel(unicode(args.y_label, 'utf8'))
-    plt.legend(fontsize='x-small', loc='upper right')
+    labels = None
+    if args.legend_labels is not None:
+        labels = args.legend_labels.split(',')
+
+    plt.legend(fontsize='x-small', loc='upper right', labels=labels)
+    if labels == ['no']:
+        plt.gca().get_legend().remove()
+    plt.gcf().set_size_inches(7,4)
     plt.savefig(args.out_img, dpi=600)
