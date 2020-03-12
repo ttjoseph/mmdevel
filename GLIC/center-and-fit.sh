190ce265a55251e1a0986ae7e1207faffcc071c3
Tom Joseph <ttjoseph@gmail.com>
2020-03-11 20:57:04 -0400

    Get rid of more project-specific files

--- GLIC/center-and-fit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-i=$1
-inp="1
-0"
-
-echo $inp | trjconv -s $i.tpr -f $i.xtc -o $i.1.tmp.xtc -pbc nojump -center
-echo 0 | trjconv -s $i.tpr -f $i.1.tmp.xtc -o $i.2.tmp.xtc -pbc res
-echo $inp | trjconv -s $i.tpr -f $i.2.tmp.xtc -o $i.nopbc.fit.xtc -fit rot+trans
-rm -f $i.1.tmp.xtc $i.2.tmp.xtc
