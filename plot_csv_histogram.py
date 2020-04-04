3a7401f6c55508b34405559cf3a59dd3c0cb9e62
Tom Joseph <ttjoseph@gmail.com>
2020-04-04 15:32:38 -0400

    Minor cosmesis

--- plot_csv_histogram.py
+++ plot_csv_histogram.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #
 # Uses matplotlib to make a histogram from data in a CSV file.
 import argparse
@@ -8,11 +8,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    ap = argparse.ArgumentParser(description='Make a histogram figure out of CSV file columns')
+    ap = argparse.ArgumentParser(description='Plot time series from CSV, with time in first column')
     ap.add_argument('csvfile', help='CSV file that contains a header with labels')
     ap.add_argument('--title', help='Title of plot')
     ap.add_argument('--xlabel', help='Label for x axis')
     ap.add_argument('--ylabel', help='Label for y axis')
+    ap.add_argument('--columns', help='Keep only these column numbers')
     args = ap.parse_args()
 
     df = pd.read_csv(args.csvfile, header=0)
@@ -26,6 +27,6 @@ if __name__ == '__main__':
     plt.ylabel(args.ylabel or '')
     plt.title(args.title or '')
     plt.legend()
-    out_filename = '%s-hist.png' % splitext(basename(args.csvfile))[0]
+    out_filename = '{}-hist.png'.format(splitext(basename(args.csvfile))[0])
     plt.savefig(out_filename)
-    print >>sys.stderr, 'Wrote histogram to %s.' % out_filename
+    print('Wrote histogram to {}.'.format(out_filename), file=sys.stderr)
