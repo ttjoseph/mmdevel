2e2a249bbb60793e9d0947e5c6d1d3a5f53cf216
Tom Joseph <ttjoseph@gmail.com>
2020-09-07 15:22:35 -0400

    Allow user to limit number of frames processed

--- ligand_occupancy.py
+++ ligand_occupancy.py
@@ -17,8 +17,10 @@ from MDAnalysis.analysis.distances import *
 if __name__ == '__main__':
     ap = argparse.ArgumentParser(description='Where do ligands go? Useful for flooding simulations')
     ap.add_argument('-c', '--cutoff', default=5, type=float, help='Distance cutoff in Angstroms')
-    ap.add_argument('-t', '--percent_frames_threshold', default=0, type=float,
+    ap.add_argument('-t', '--percent-frames-threshold', default=0, type=float,
         help='Only print residues with this percent occupancy or greater (out of 100)')
+    ap.add_argument('-n', '--num-frames', type=int,
+        help='Limit the number of frames processed (beginning at the first frame)')
     ap.add_argument('-g', '--group-across-chains', action='store_true', default=False,
                     help='Group counts across chains (useful when the protein is multiple identical monomers)')
     ap.add_argument('--colorize-pdb-filename', help='Filename for output suitable for colorize_pdb.py (percent occupancy cutoff ignored)')
@@ -34,14 +36,19 @@ if __name__ == '__main__':
 
     num_residues = len(protein_ca)
     first_resid = protein_ca.residues[0].resid
+    num_frames = args.num_frames or len(u.trajectory)
     print('Trajectory files: %s' % ', '.join(args.dcd), file=sys.stderr)
     print('Residues: %d' % num_residues, file=sys.stderr)
     print('First resid: %d' % first_resid, file=sys.stderr)
-    print('Frames: %d' % len(u.trajectory), file=sys.stderr)
+    print('Frames: To process %d of %d' % (num_frames, len(u.trajectory)), file=sys.stderr)
     print('Distance cutoff: %.2f Å for resname %s' % (args.cutoff, args.ligand_resname), file=sys.stderr)
     print('Percent occupancy threshold: %.2f%%' % args.percent_frames_threshold, file=sys.stderr)
     counts = np.zeros(num_residues)
-    for ts in tqdm(u.trajectory):
+    for ts in tqdm(u.trajectory, unit='frames'):
+        if ts.frame >= num_frames:
+            print(f"Stopping here since you only asked for the first {num_frames} frames.", file=sys.stderr)
+            break
+
         d = distance_array(protein_ca.positions, ligand.positions, box=ts.dimensions)
         resid = 0
         for ca in d:
@@ -61,12 +68,12 @@ if __name__ == '__main__':
             counts_coalesced[key] += counts[i]
 
         for key in sorted(list(counts_coalesced.keys()), key=lambda res: int(res[3:])):
-            percent_frames = 100*float(counts_coalesced[key])/len(u.trajectory)
+            percent_frames = 100*float(counts_coalesced[key])/num_frames
             if counts_coalesced[key] > 0 and percent_frames > args.percent_frames_threshold:
                 print('%d\t%.2f%%\t%s' % (counts_coalesced[key], percent_frames, key))
     else:
         for i in range(len(counts)):
-            percent_frames = 100*float(counts[i])/len(u.trajectory)
+            percent_frames = 100*float(counts[i])/num_frames
             if counts[i] > 0 and percent_frames > args.percent_frames_threshold:
                 print('%d\t%.2f%%\t%s%d' % (counts[i], percent_frames, protein_ca[i].resname, protein_ca[i].resid))
 
