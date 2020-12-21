44c9bebe899dfa617b46c256335966c3f21817f2
Tom Joseph <ttjoseph@gmail.com>
2020-12-21 13:04:29 -0500

    A number of quality of life improvements

--- regenerate_psf.py
+++ regenerate_psf.py
@@ -14,8 +14,7 @@ import argparse
 import pathlib
 import tempfile
 import subprocess
-import MDAnalysis as mda
-
+from libttj import ShadyPDB
 
 def main():
     ap = argparse.ArgumentParser(description='Remakes a PSF/PDB from existing PDB. Helpful if you want to avoid the CHARMM-GUI rigamarole')
@@ -23,7 +22,7 @@ def main():
     ap.add_argument('toppar_txt', help='File containing newline-separated list of psfgen-acceptable topology files')
     ap.add_argument('out_prefix', help='Prefix for output PSF and PDB files')
     ap.add_argument('--chunk-size', type=int, default=9999, help='Maximum number of residues in a segment that you think psfgen or whatever can handle')
-    ap.add_argument('--show-vmd-output', action='store_true', help='Show the output of VMD, which may be long')
+    ap.add_argument('--vmd-output-file', help='Save the output of VMD in this file')
     ap.add_argument('--show-psfgen-script', action='store_true', help='Show the psfgen script that will be passed to VMD')
     ap.add_argument('--vmdbin', default='vmd', help='VMD binary')
     ap.add_argument('--keep-temp-pdbs', action='store_true', help='Keep the temporary PDBs made for psfgen so you can look at them')
@@ -34,52 +33,65 @@ def main():
     # splitting here, in Python. (This often is necessary when there is a bunch of water in a simulation system.)
     out_seg_suffixes = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-    u = mda.Universe(args.pdb)
+    # u = mda.Universe(args.pdb)
+    u = ShadyPDB(args.pdb)
     segs_to_process = []
-    for seg_i in range(len(u.segments)):
-        seg = u.segments[seg_i]
-        out_seg_prefix = seg.segid[0:3].strip()
+    seg_i = 0
 
-        num_chunks = int(len(seg.residues) / args.chunk_size)
-        if len(seg.residues) % args.chunk_size != 0:
+    for segid, resindexes in u.segid_to_resindex.items():
+        out_seg_prefix = segid[0:3].strip()
+
+        num_chunks = int(len(resindexes) / args.chunk_size)
+        if len(resindexes) % args.chunk_size != 0:
             num_chunks += 1
 
         if num_chunks == 1:
-            out_segid = seg.segid
+            out_segid = segid
             # We have to say delete=False here because the temporary file's variable may be GCed before we invoke VMD below,
             # which will cause the file to be deleted
             tmp = tempfile.NamedTemporaryFile(prefix=f'{out_segid}_', suffix='.pdb', delete=False)
-            seg.atoms.write(tmp.name)
-            print(f"Filename for segid #{seg_i} for {seg.segid} is {tmp.name}", file=sys.stderr)
+            # Replacement for seg.atoms.write(tmp.name)
+            chunk_atomindexes = []
+            for resindex in resindexes:
+                chunk_atomindexes.extend(u.resindex_to_atomindex[resindex])
+            print(f"Filename for segid #{seg_i} for {segid} ({len(chunk_atomindexes)} atoms) is {tmp.name}", file=sys.stderr)
+            u.renumber_subset(chunk_atomindexes)
+            u.write_to_pdb(tmp, chunk_atomindexes)
             segs_to_process.append((out_segid, tmp.name))
             continue
 
-        # Sanity check for fabulously large segment
+        # Sanity check for fabulously large segment where we've run out of segment name suffix characters
         if num_chunks > len(out_seg_suffixes):
-            print(f"You need {num_chunks} chunks for segment {seg.segid} but I am only smart enough for {len(out_seg_suffixes)}", file=sys.stderr)
+            print(f"Error: You need {num_chunks} chunks for segment {segid} but I am only smart enough for {len(out_seg_suffixes)}", file=sys.stderr)
             exit(1)
 
+        # We would only reach here if num_chunks > 1 (note continue statement above)
         for chunk_i in range(num_chunks):
             out_segid = f"{out_seg_prefix}{out_seg_suffixes[chunk_i]}"
 
             length = args.chunk_size
             offset = chunk_i * length
-            if (offset + length) >= len(seg.residues):
-                length = len(seg.residues) - offset
+            if (offset + length) >= len(resindexes):
+                length = len(resindexes) - offset
             
             # print(f"{seg.segid} (chunk #{chunk_i}) -> {out_segid}: residue offset = {offset}, length = {length}", file=sys.stderr)            
             tmp = tempfile.NamedTemporaryFile(prefix=f'{out_segid}_', suffix='.pdb', delete=False)
-            print(f"Filename for segid #{seg_i} chunk #{chunk_i} for {seg.segid} is {tmp.name}", file=sys.stderr)
 
-            # Apparently, we can't set the segid of an AtomGroup, but we can of a segment?
-            # This does not work: seg.residues[offset:offset+length].atoms.segid = out_segid
-            old_segid = seg.segid
-            seg.segid = out_segid
-            seg.residues[offset:offset+length].atoms.write(tmp.name)
-            seg.segid = old_segid
+            # Get all the atom indexes for this chunk then process them
+            # seg.residues[offset:offset+length].atoms.write(tmp.name)
+            # Write this subset of atoms with a custom segid
+            # offset is relative to the first residue in this segment, where offset 0 is the first residue
+            chunk_atomindexes = []
+            for resindex in resindexes[offset:offset+length]:
+                chunk_atomindexes.extend(u.resindex_to_atomindex[resindex])
+            
+            print(f"Filename for segid #{seg_i} chunk #{chunk_i} for {segid} ({len(chunk_atomindexes)} atoms)is {tmp.name}", file=sys.stderr)
+            u.renumber_subset(chunk_atomindexes)
+            u.set_segid(chunk_atomindexes, out_segid)
+            u.write_to_pdb(tmp, chunk_atomindexes)
             
             segs_to_process.append((out_segid, tmp.name))
-
+        seg_i += 1
 
     # Convert a newline-separated list of files to psfgen commands
     toppar_cmds = '\n'.join([f"topology {x.strip()}" for x in open(args.toppar_txt).readlines()])
@@ -110,13 +122,14 @@ writepdb {args.out_prefix}.pdb
     # Run vmd -dispdev text -eofexit
     print('Running VMD to do the actual PSF/PDB generation. Fasten your seatbelt...', file=sys.stderr)
     c = subprocess.run([args.vmdbin, '-dispdev', 'text', '-eofexit'], input=bytes(psfgen_script, 'utf-8'), capture_output=True)
-    print(f'Done running VMD. I hope it worked! If any doubt, run me again with --show-vmd-output.', file=sys.stderr)
+    print(f'Done running VMD. I hope it worked!', file=sys.stderr)
     
-    if args.show_vmd_output:
-        print('VMD stdout:\n===========', file=sys.stderr)
-        print(str(c.stdout, 'utf-8'))
-        print('\nVMD stderr:\n===========', file=sys.stderr)
-        print(str(c.stderr, 'utf-8'))
+    if args.vmd_output_file is not None:
+        with open(args.vmd_output_file, 'w') as f:
+            print('VMD stdout:\n===========', file=f)
+            print(str(c.stdout, 'utf-8'), file=f)
+            print('\nVMD stderr:\n===========', file=f)
+            print(str(c.stderr, 'utf-8'), file=f)
 
     # Delete all those temporary files
     if args.keep_temp_pdbs is False:
