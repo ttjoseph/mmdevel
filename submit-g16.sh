b543beffd14b8b21b6494c8b0e4fd883cf477794
Tom Joseph <ttjoseph@gmail.com>
2023-03-07 15:21:31 -0500

    G16 lives on /nas1

--- submit-g16.sh
+++ submit-g16.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 # Conveniently submit a Gaussian job to the friendly local SLURM.
 # Usage: submit-g16.sh whatever.gau
-nproc=`grep -i nproc $1 | cut -d= -f2`
+# nproc=`grep -i nproc $1 | cut -d= -f2`
 sbatch <<FOO
 #!/bin/bash
 #SBATCH -J $1
-#SBATCH -n $nproc
+#SBATCH -n 32
+
+export g16root=/nas1/sw
+. \$g16root/g16/bsd/g16.profile
 
 g16 $1
-echo "Done with $1 in \$PWD" | ~/bin/slack-ttjoseph
+echo "Done with $1 in \$PWD" | /nas1/sw/bin/slack-ttjoseph
 FOO
