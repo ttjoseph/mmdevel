8ce948d9fc25117a7bcc3f16a5f0a46504b37a40
Tom Joseph <ttjoseph@gmail.com>
2024-07-07 09:53:15 -0400

    Swap docstring-like documentation for multiline comment to avoid warnings about escaped characters

--- relentless_fep.py
+++ relentless_fep.py
@@ -1,172 +1,172 @@
 #!/usr/bin/env python3
-"""Relentless FEP
+# Relentless FEP
+# 
+# Eases administrative burden of large FEP calculations by automating, to some degree, the
+# resumption of interrupted NAMD runs.
+# 
+# Given a description of the overall FEP calculation and a particular window's .fepout files,
+# generates a new .namd file that starts or continues the calculation for this window.
+# Intended to tolerate unexpectedly interrupted calculations, such as when your jobs are
+# preempted.
+# 
+# "Quick" start
+# -------------
+# 
+# This script is only one part of a FEP calculation infrastructure. All it does is examine
+# the output of a single FEP window run (if it exists), then generates a NAMD configuration
+# file that, when run to completion, will finish that window. You are expected to run the
+# windows in order yourself, which usually means writing a batch job submission script.
+# 
+# The FEP calculation is divided into groups of windows. Each group begins from the same
+# initial simulation state. This allows for better parallelization at the cost of more
+# overall computation required due to an increased need for equilibration. For example,
+# if your FEP calculation needs 120 windows, running all the windows in sequence will take
+# a long time. But if you can run 0-4, 5-9, 10-14, etc. simultaneously on your cluster, you
+# can save a lot of wallclock time.
+# 
+# Of course, if you want to run all 120 windows sequentially, you can just say that each
+# group should have 121 or more windows. (That last window is the final IDWS window in the
+# reverse direction.)
+# 
+# Each FEP calculation requires:
+# 
+#   1. A configuration file in YAML format listing your requirements for the entire
+#      calculation
+#   2. A NAMD configuration that will be included in each generated NAMD configuration file
+#   3. Associated files such as PSF/PDB, parameter files, colvars files, and whatever else
+#      your particular simulations need
+# 
+# You must choose a prefix string that identifies your YAML and NAMD config files. For
+# example, if you choose the prefix 'myfep', your YAML file must be named 'myfep.yaml' and 
+# your NAMD config 'myfep.include.namd'. The output files will have the form 'myfep000.fepout',
+# 'myfep000.log', 'myfep000.colvars.traj', etc.
+# 
+# Here is an example YAML file, with 21 total windows (including the final reverse windows),
+# split into groups of 5:
+# 
+# ```
+# lambda_schedule: [0, 0.1, 0.12, 0.17, 0.2, 0.22, 0.25, 0.3, 0.35, 0.4, 0.5, 0.55, 0.6, 0.65, 0.7, 0.8, 0.9, 0.95, 0.98, 0.99, 1.0]
+# start_config_prefix: mineq
+# restart_frequency: 10000
+# total_steps_per_window: 1500000
+# windows_per_group: 5
+# minimize: 100
+# per_group_equil_steps: 500000
+# per_window_equil_steps: 200000
+# ```
+# 
+# And a description of the parameters:
+#   - "lambda_schedule" must span 0 to 1.0. Or, you can just write a single number if you want that
+#     many evenly-spaced windows.
+#   - "start_config_prefix" identifies the simulation input to begin from with the first window in
+#     each group. Here this means mineq.coor, mineq.vel, mineq.xsc. You will specify the PSF file
+#     in the NAMD config myfep.include.namd.
+#   - "restart_frequency" is the NAMD restart frequency that you specified in the NAMD config. This
+#     is important so relentless_fep can determine exactly how many steps are necessary to run
+#     when a simulation is interrupted and resumed.
+#   - "total_steps_per_window" is the goal number of simulation steps per FEP window. In this case,
+#     assuming a 2 fs timestep, each window would be 3 ns. This includes both equilibration and
+#     production simulation.
+#   - "windows_per_group" specifies how many windows each group should contain. A group of windows
+#     can be run in parallel with the other groups of windows, because each group starts from
+#     the same simulation configuration (here, mineq.coor, mineq.vel, mineq.xsc).
+#   - "minimize" allows for minimization of the simulation prior to dynamics for only the first
+#     window in each group. This is useful if you edit the simulation input system by hand, such as
+#     to modify a functional group.
+#   - "per_group_equil_steps" is the number of equilibration steps to run at the start of the
+#     group. This is *not* in addition to the per_window_equil_steps. This is the value passed to
+#     NAMD using the alchEquilSteps option.
+#   - "per_window_equil_steps" is the number of equilibration steps for all windows except the first
+#     one in a group.
+# 
+# When relentless_fep.py is run for window 0 as follows:
+# 
+# ```
+# $ python3 relentless_fep.py myfep 0
+# ```
+# 
+# ...it will generate a NAMD config file named myfep000.namd that looks like this:
+# 
+# ```
+# # FEP from 0.0 to 0.1 (IDWS None)
+# set inputname mineq
+# set outputname myfep000
+# alchLambda 0.0
+# alchLambda2 0.1
+# source myfep.include.namd
+# firsttimestep 0
+# minimize 100
+# alchEquilSteps 500000
+# run 1500000
+# ```
+# 
+# Your myfep.include.namd should include everything else NAMD needs to run your simulation. This
+# would include whatever colvars parameters you need and so forth.
+# 
+# Let's say you ran this simulation, but then you had a power outage after 400000 steps. All you
+# now must do is re-run relentless_fep.py in exactly the same way as before:
+# 
+# ```
+# $ python3 relentless_fep.py myfep 0
+# ```
+# 
+# It will generate a NAMD config file named myfep000a.namd that looks like this:
+# 
+# ```
+# # FEP from 0.0 to 0.1 (IDWS None)
+# set inputname myfep000.restart
+# set outputname myfep000a
+# alchLambda 0.0
+# alchLambda2 0.1
+# source myfep.include.namd
+# firsttimestep 400000
+# alchEquilSteps 100000
+# run 1100000
+# ```
+# 
+# Naturally, it is cumbersome to run these simulations by hand. So, you should write a batch
+# submission script that automates the process. Since compute clusters vary in how you must
+# interact with them, this is unfortunately highly dependent on your local setup. But, as an
+# example, here is the skeleton of such a script that would work with the SLURM batch queue:
+# 
+# ```
+# #!/bin/bash
+# NAMD=$HOME/bin/namd2
+# PREFIX=myfep
+# FIRST=0
+# LAST=20
+# GROUP_SIZE=5
+# for group_start in `seq $FIRST $GROUP_SIZE $LAST`; do
+#     group_end=$(($group_start + $GROUP_SIZE - 1))
+#     if (( $group_end > $LAST )); then
+#         $group_end = $LAST
+#     fi
+# 
+#     SCRIPT() { cat <<EOF
+# #!/bin/bash
+# #SBATCH -J ${PREFIX}_${group_start}_${group_end}
+# for m in \$(seq $group_start $group_end); do
+#     m=$(printf %03d \$m)
+#     namdconf=\$(python3 relentless_fep.py ${PREFIX}.yaml \${m})
+#     # If relentless_fep did not generate a NAMD config file, it's done with this window,
+#     # so don't try to run it
+#     if [ -z \$namdconf ]; then continue; fi
+#     namdlog=\$(basename \$namdconf .namd).log
+#     $NAMD \${namdconf} > \${namdlog}
+# done
+# EOF
+#     }
+# 
+#     # Submit the job
+#     SCRIPT | sbatch
+# done
+# ```
+# 
+# If a calcuation fails, you can just resubmit the script without any other changes. Often
+# the batch queue system can be configured to do this automatically for you. Before using
+# this script, be sure to modify it for your particular cluster, in particular specifying
+# now many CPUs to use (this script will only use 1!).
 
-Eases administrative burden of large FEP calculations by automating, to some degree, the
-resumption of interrupted NAMD runs.
-
-Given a description of the overall FEP calculation and a particular window's .fepout files,
-generates a new .namd file that starts or continues the calculation for this window.
-Intended to tolerate unexpectedly interrupted calculations, such as when your jobs are
-preempted.
-
-"Quick" start
--------------
-
-This script is only one part of a FEP calculation infrastructure. All it does is examine
-the output of a single FEP window run (if it exists), then generates a NAMD configuration
-file that, when run to completion, will finish that window. You are expected to run the
-windows in order yourself, which usually means writing a batch job submission script.
-
-The FEP calculation is divided into groups of windows. Each group begins from the same
-initial simulation state. This allows for better parallelization at the cost of more
-overall computation required due to an increased need for equilibration. For example,
-if your FEP calculation needs 120 windows, running all the windows in sequence will take
-a long time. But if you can run 0-4, 5-9, 10-14, etc. simultaneously on your cluster, you
-can save a lot of wallclock time.
-
-Of course, if you want to run all 120 windows sequentially, you can just say that each
-group should have 121 or more windows. (That last window is the final IDWS window in the
-reverse direction.)
-
-Each FEP calculation requires:
-
-  1. A configuration file in YAML format listing your requirements for the entire
-     calculation
-  2. A NAMD configuration that will be included in each generated NAMD configuration file
-  3. Associated files such as PSF/PDB, parameter files, colvars files, and whatever else
-     your particular simulations need
-
-You must choose a prefix string that identifies your YAML and NAMD config files. For
-example, if you choose the prefix 'myfep', your YAML file must be named 'myfep.yaml' and 
-your NAMD config 'myfep.include.namd'. The output files will have the form 'myfep000.fepout',
-'myfep000.log', 'myfep000.colvars.traj', etc.
-
-Here is an example YAML file, with 21 total windows (including the final reverse windows),
-split into groups of 5:
-
-```
-lambda_schedule: [0, 0.1, 0.12, 0.17, 0.2, 0.22, 0.25, 0.3, 0.35, 0.4, 0.5, 0.55, 0.6, 0.65, 0.7, 0.8, 0.9, 0.95, 0.98, 0.99, 1.0]
-start_config_prefix: mineq
-restart_frequency: 10000
-total_steps_per_window: 1500000
-windows_per_group: 5
-minimize: 100
-per_group_equil_steps: 500000
-per_window_equil_steps: 200000
-```
-
-And a description of the parameters:
-  - "lambda_schedule" must span 0 to 1.0. Or, you can just write a single number if you want that
-    many evenly-spaced windows.
-  - "start_config_prefix" identifies the simulation input to begin from with the first window in
-    each group. Here this means mineq.coor, mineq.vel, mineq.xsc. You will specify the PSF file
-    in the NAMD config myfep.include.namd.
-  - "restart_frequency" is the NAMD restart frequency that you specified in the NAMD config. This
-    is important so relentless_fep can determine exactly how many steps are necessary to run
-    when a simulation is interrupted and resumed.
-  - "total_steps_per_window" is the goal number of simulation steps per FEP window. In this case,
-    assuming a 2 fs timestep, each window would be 3 ns. This includes both equilibration and
-    production simulation.
-  - "windows_per_group" specifies how many windows each group should contain. A group of windows
-    can be run in parallel with the other groups of windows, because each group starts from
-    the same simulation configuration (here, mineq.coor, mineq.vel, mineq.xsc).
-  - "minimize" allows for minimization of the simulation prior to dynamics for only the first
-    window in each group. This is useful if you edit the simulation input system by hand, such as
-    to modify a functional group.
-  - "per_group_equil_steps" is the number of equilibration steps to run at the start of the
-    group. This is *not* in addition to the per_window_equil_steps. This is the value passed to
-    NAMD using the alchEquilSteps option.
-  - "per_window_equil_steps" is the number of equilibration steps for all windows except the first
-    one in a group.
-
-When relentless_fep.py is run for window 0 as follows:
-
-```
-$ python3 relentless_fep.py myfep 0
-```
-
-...it will generate a NAMD config file named myfep000.namd that looks like this:
-
-```
-# FEP from 0.0 to 0.1 (IDWS None)
-set inputname mineq
-set outputname myfep000
-alchLambda 0.0
-alchLambda2 0.1
-source myfep.include.namd
-firsttimestep 0
-minimize 100
-alchEquilSteps 500000
-run 1500000
-```
-
-Your myfep.include.namd should include everything else NAMD needs to run your simulation. This
-would include whatever colvars parameters you need and so forth.
-
-Let's say you ran this simulation, but then you had a power outage after 400000 steps. All you
-now must do is re-run relentless_fep.py in exactly the same way as before:
-
-```
-$ python3 relentless_fep.py myfep 0
-```
-
-It will generate a NAMD config file named myfep000a.namd that looks like this:
-
-```
-# FEP from 0.0 to 0.1 (IDWS None)
-set inputname myfep000.restart
-set outputname myfep000a
-alchLambda 0.0
-alchLambda2 0.1
-source myfep.include.namd
-firsttimestep 400000
-alchEquilSteps 100000
-run 1100000
-```
-
-Naturally, it is cumbersome to run these simulations by hand. So, you should write a batch
-submission script that automates the process. Since compute clusters vary in how you must
-interact with them, this is unfortunately highly dependent on your local setup. But, as an
-example, here is the skeleton of such a script that would work with the SLURM batch queue:
-
-```
-#!/bin/bash
-NAMD=$HOME/bin/namd2
-PREFIX=myfep
-FIRST=0
-LAST=20
-GROUP_SIZE=5
-for group_start in `seq $FIRST $GROUP_SIZE $LAST`; do
-    group_end=$(($group_start + $GROUP_SIZE - 1))
-    if (( $group_end > $LAST )); then
-        $group_end = $LAST
-    fi
-
-    SCRIPT() { cat <<EOF
-#!/bin/bash
-#SBATCH -J ${PREFIX}_${group_start}_${group_end}
-for m in \`seq $group_start $group_end\`; do
-    m=\`printf %03d \$m`
-    namdconf=\$(python3 relentless_fep.py ${PREFIX}.yaml \${m})
-    # If relentless_fep did not generate a NAMD config file, it's done with this window,
-    # so don't try to run it
-    if [ -z \$namdconf ]; then continue; fi
-    namdlog=\$(basename \$namdconf .namd).log
-    $NAMD \${namdconf} > \${namdlog}
-done
-EOF
-    }
-
-    # Submit the job
-    SCRIPT | sbatch
-done
-```
-
-If a calcuation fails, you can just resubmit the script without any other changes. Often
-the batch queue system can be configured to do this automatically for you. Before using
-this script, be sure to modify it for your particular cluster, in particular specifying
-now many CPUs to use (this script will only use 1!).
-"""
 import argparse
 import sys
 import re
